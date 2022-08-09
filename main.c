369e254882450ddfe65b42888778c3b4eb6cc8a2
Pelumi <odunoyepelumi23@gmail.com>
2022-08-09 03:29:44 -0700

    f

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+#include "main.h"
+
+
+void free_data(data_shell *datash)
+{
+	unsigned int c;
+
+	for (c = 0; datash[c]; c++)
+	{
+		free(datash[c];
+				}
+
+/**7ncomplete functions
+ */
+
+void set_data /**arguments/
+
+
+int main		
+
 
